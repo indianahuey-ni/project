dbe3c64b4761686cb34d93c89c8634b86f46a88b
Indiana Huey <Indiana.Huey@tech180.us>
2024-05-29 15:04:00 -0400

    Project template

---
+++ generate_artifacts.py
@@ -0,0 +1,5 @@
+from designer.tracer import Tracer
+
+
+if __name__ == '__main__':
+    tracer = Tracer(0)
---
+++ pull.bat
@@ -0,0 +1,2 @@
+git pull origin main
+git pull https://github.com/indianahuey-ni/designer.git main --allow-unrelated-histories
